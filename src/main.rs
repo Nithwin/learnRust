19ff4f20860cfad230e0a8ecdc46dcac0e575842
Nithwin <vmnithwin@gmail.com>
2026-01-01 22:54:22 +0530

    Match Completed

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,48 @@
 
-use std::io::{self, Write};
+use std::io;
 
 
 fn main(){
-    let mut age: String = String::new();
-    let mut name: String = String::new();
-    print!("Enter your name: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut name).expect("Failed to get input");
-    print!("Enter your age: ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut age).expect("Failed to get input");
-    let name = name.trim();
-    let age: i32 = age.trim().parse().expect("please enter the number");
-    println!("Your name is {} and your age is {}", name, age);
-
+    let mut input=String::new();
+    io::stdin().read_line(&mut input).expect("Failed");
+    let input: i32 = input.trim().parse().expect("Sorry you have entered string");
+    let res = match input {
+        1 => "Monday",
+        2 => "Tuesday",
+        3 => "Wednesday",
+        4 => "Thursday",
+        5 => "Friday",
+        6 => "Saturday",
+        7 => "Sunday",
+        _ => "Sunday",
+    };
+    println!("{}", res);
+    // match input {
+    //     1 => print!("Monday"),
+    //     2 => print!("Tuesday"),
+    //     3 => print!("Wednesday"),
+    //     4 => print!("Thursday"),
+    //     5 => print!("Friday"),
+    //     6 => print!("Saturday"),
+    //     7 => print!("Sunday"),
+    //     _ => print!("Wrong"),
+    // };
 }
+
+// fn main(){
+//     let mut age: String = String::new();
+//     let mut name: String = String::new();
+//     print!("Enter your name: ");
+//     io::stdout().flush().unwrap();
+//     io::stdin().read_line(&mut name).expect("Failed to get input");
+//     print!("Enter your age: ");
+//     io::stdout().flush().unwrap();
+//     io::stdin().read_line(&mut age).expect("Failed to get input");
+//     let name = name.trim();
+//     let age: i32 = age.trim().parse().expect("please enter the number");
+//     println!("Your name is {} and your age is {}", name, age);
+
+// }
 
 
 // fn main(){
