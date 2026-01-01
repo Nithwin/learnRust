0c2407137383de2059f1a19a76cb9ae17558f5b3
Nithwin <vmnithwin@gmail.com>
2026-01-01 23:14:15 +0530

    Loops

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,52 @@ use std::io;
 
 
 fn main(){
-    let mut input=String::new();
-    io::stdin().read_line(&mut input).expect("Failed");
-    let input: i32 = input.trim().parse().expect("Sorry you have entered string");
-    let res = match input {
-        1 => "Monday",
-        2 => "Tuesday",
-        3 => "Wednesday",
-        4 => "Thursday",
-        5 => "Friday",
-        6 => "Saturday",
-        7 => "Sunday",
-        _ => "Sunday",
-    };
-    println!("{}", res);
-    // match input {
-    //     1 => print!("Monday"),
-    //     2 => print!("Tuesday"),
-    //     3 => print!("Wednesday"),
-    //     4 => print!("Thursday"),
-    //     5 => print!("Friday"),
-    //     6 => print!("Saturday"),
-    //     7 => print!("Sunday"),
-    //     _ => print!("Wrong"),
-    // };
+    // loop {
+    //     print!("Akhila... ")
+    // }
+    //let mut count = 0;
+    // loop {
+    //     count += 1;
+    //     print!("{} \n", count);
+    //     if count == 100000 {
+    //         break;
+    //     }
+    // }
+    // while count <= 1000 {
+    //     print!("{}\n", count);
+    //     count += 1;
+    // }
+    // for i in 0..=100 {
+    //     print!("{}\n", i);
+    // }
 }
+
+// fn main(){
+//     let mut input=String::new();
+//     io::stdin().read_line(&mut input).expect("Failed");
+//     let input: i32 = input.trim().parse().expect("Sorry you have entered string");
+//     let res = match input {
+//         1 => "Monday",
+//         2 => "Tuesday",
+//         3 => "Wednesday",
+//         4 => "Thursday",
+//         5 => "Friday",
+//         6 => "Saturday",
+//         7 => "Sunday",
+//         _ => "Sunday",
+//     };
+//     println!("{}", res);
+//     // match input {
+//     //     1 => print!("Monday"),
+//     //     2 => print!("Tuesday"),
+//     //     3 => print!("Wednesday"),
+//     //     4 => print!("Thursday"),
+//     //     5 => print!("Friday"),
+//     //     6 => print!("Saturday"),
+//     //     7 => print!("Sunday"),
+//     //     _ => print!("Wrong"),
+//     // };
+// }
 
 // fn main(){
 //     let mut age: String = String::new();
